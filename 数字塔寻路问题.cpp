e868c624c8132eb90ebe77963d069453fe545808
Liu Zicheng <90845197+58120201@users.noreply.github.com>
2022-09-11 16:31:06 +0800

    Update 数字塔寻路问题.cpp

--- 数字塔寻路问题.cpp
+++ 数字塔寻路问题.cpp
@@ -1,3 +1,5 @@
+//寻找数字塔（堆）的路径和积累最大值
+//将堆转换成矩阵进行维护，动态规划得出结果
 #include<iostream>
 #include<string>
 using namespace std;
@@ -14,8 +16,8 @@ int main()
 	for(int i=3;i>=0;i--)
 		for(int j=3-i;j>=0;j--)
 			a[i][j]=b[i][j]+max(a[i+1][j],a[i][j+1]);
-	cout<<"����Ϊ��"<<a[0][0]<<endl;
-	cout<<"·��Ϊ��"<<b[0][0];
+	cout<<"最大和为："<<a[0][0]<<endl;
+	cout<<"路径为："<<b[0][0];
     for(int n=1,i=0;n<=4;n++)
 	{
 		i=(a[i+1][n-i-1]>a[i][n-i])?i+1:i;
